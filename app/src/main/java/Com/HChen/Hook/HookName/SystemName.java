9740fe80315c471f91f95a0ae6346702c34643d2
HChenX <xhc-xh@qq.com>
2023-08-25 17:19:59 +0800

    Recovery library

---
+++ app/src/main/java/Com/HChen/Hook/HookName/SystemName.java
@@ -0,0 +1,21 @@
+package Com.HChen.Hook.HookName;
+
+public final class SystemName {
+    public static final String checkExcessivePowerUsage = "checkExcessivePowerUsage";
+    public static final String killProcessesBelowAdj = "killProcessesBelowAdj";
+    public static final String performIdleMaintenance = "performIdleMaintenance";
+    public static final String getAppStartModeLOSP = "getAppStartModeLOSP";
+    public static final String killAppIfBgRestrictedAndCachedIdleLocked = "killAppIfBgRestrictedAndCachedIdleLocked";
+    public static final String runKillAll = "runKillAll";
+    public static final String shouldKillExcessiveProcesses = "shouldKillExcessiveProcesses";
+    public static final String updateAndTrimProcessLSP = "updateAndTrimProcessLSP";
+    public static final String trimPhantomProcessesIfNecessary = "trimPhantomProcessesIfNecessary";
+    public static final String trimInactiveRecentTasks = "trimInactiveRecentTasks";
+    public static final String isInVisibleRange = "isInVisibleRange";
+    public static final String run = "run";
+    public static final String getDefaultMaxCachedProcesses = "getDefaultMaxCachedProcesses";
+    public static final String updateMaxCachedProcesses = "updateMaxCachedProcesses";
+    public static final String updateKillBgRestrictedCachedIdle = "updateKillBgRestrictedCachedIdle";
+    public static final String updateLowMemStateLSP = "updateLowMemStateLSP";
+    public static final String setOverrideMaxCachedProcesses = "setOverrideMaxCachedProcesses";
+}
