c478cb908868388bf051afbd7bbef149aaaa3b4b
焕晨HChen <HChen_LXi@outlook.com>
2024-01-26 17:04:40 +0800

    添加许可头
    Signed-off-by: 焕晨HChen <HChen_LXi@outlook.com>

--- app/src/main/java/Com/HChen/Hook/Utils/SystemLog.java
+++ app/src/main/java/Com/HChen/Hook/Utils/SystemLog.java
@@ -1,3 +1,25 @@
+/*
+ * This file is part of AppRetentionHook.
+
+ * AppRetentionHook is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License.
+
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+ * Author of this project: 焕晨HChen
+ * You can reference the code of this project,
+ * but as a project developer, I hope you can indicate it when referencing.
+
+ * Copyright (C) 2023-2024 AppRetentionHook Contributions
+ */
 package Com.HChen.Hook.Utils;
 
 import android.util.Log;
